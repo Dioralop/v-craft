402b004ff71b19bd983bee9ad53b4e639e6b6a7d
Dioralop <Dioralop@users.noreply.github.com>
2021-06-07 09:59:30 +0100

    Exposing more features

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import Blueprint from '@/components/Blueprint.vue';
 import Canvas from '@/components/Canvas.vue';
 import Editor from '@/components/Editor.vue';
 import Frame from '@/components/Frame.vue';
+import Node from '@/components/Node.vue';
 import settingMixin from '@/components/settingMixin';
+import createNodeFromVNode from '@/utils/createNodeFromVNode.js';
 
 export {
   Blueprint,
   Canvas,
   Editor,
   Frame,
+  Node,
   settingMixin,
+  createNodeFromVNode,
 };
